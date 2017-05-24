0cd65be3395d5c4adfb249d93e7b1faaf1050af8
Amit Jain <gravicle@gmail.com>
2017-05-23 21:19:41 -0700

    Remove client code

--- fhir-parser-resources/FHIRAbstractResource.swift
+++ fhir-parser-resources/FHIRAbstractResource.swift
@@ -17,14 +17,6 @@ open class FHIRAbstractResource: FHIRAbstractBase {
 	/// A specific version id, if the instance was created using `vread`.
 	public var _versionId: String?
 	
-	/// If this instance lives on a server, this property represents that server.
-	public var _server: FHIRServer? {
-		get { return __server ?? _owningResource?._server }
-		set { __server = newValue }
-	}
-	var __server: FHIRServer?
-	
-	
 	// MARK: - FHIRJSONType
 	
 	/**
@@ -76,7 +68,7 @@ open class FHIRAbstractResource: FHIRAbstractBase {
 	// MARK: - CustomStringConvertible
 	
 	override open var description: String {
-		return "<\(type(of: self).resourceType)> \(__server?.baseURL.absoluteString ?? "nil")"
+		return "<\(type(of: self).resourceType)>"
 	}
 }
 
